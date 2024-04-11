1269f3ff1ec66219c23f1aefcad61769f4190744
pirabaud <rabaudp@gmail.com>
2024-04-11 15:28:52 +0200

    feat(front): start front

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const { pool } = require('./database');
 
 const express = require('express'), 
 app = express(); 
+app.use(cors())
   
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.json()) 
@@ -11,6 +12,10 @@ app.get('/',
       const data = await pool.query("SELECT * FROM users;");
       res.send(data);
    }) 
+
+app.post('/', async (req, res) => {
+   console.log(req)
+})
   
 app.listen(5000,  
    () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
