0038fe3f0fa3bce63f2f846265d7ea4f62d70ee3
Arthur Faby <arthurfaby1@gmail.com>
2024-04-12 10:46:51 +0200

    fix(query): change return type

--- backend/libs/orm/queries/query.ts
+++ backend/libs/orm/queries/query.ts
@@ -1,6 +1,7 @@
 import pool from "../../../database";
+import { APIResponse } from "../types/response_type";
 
-const query = async <T>(query: string): Promise<any> => {
+const query = async <T>(query: string): Promise<APIResponse<T>> => {
     return await pool.query(query);
 }
 
