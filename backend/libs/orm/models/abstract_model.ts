21099d622a54a3b9d8a3473909ecfcfe58822958
Arthur Faby <arthurfaby1@gmail.com>
2024-04-15 18:16:38 +0200

    feat(backend:orm): edit abstract model

--- backend/libs/orm/models/abstract_model.ts
+++ backend/libs/orm/models/abstract_model.ts
@@ -1,12 +1,31 @@
+import { AbstractDto } from "../dtos/abstract_dto";
+import query from "../queries/abstract_query";
 import select from "../queries/select_query";
 import { Filters } from "../types/filter_type";
 
-export abstract class AbstractModel<T> {
+export abstract class AbstractModel<T extends AbstractDto> {
     /**
      * The table name
      * @type {string}
      * @static
      * @public
      */
-    public static tableName: string;
+    public tableName: string;
+
+    public dto: T;
+
+    /**
+     * @constructor
+     * @param {T} dto The data transfer object
+     * @param {string} tableName The table name
+     * @public
+     */
+    constructor(dto: T, tableName: string) {
+        this.dto = dto;
+        this.tableName = tableName;
+    }
+
+    public async delete() {
+        return await query(`DELETE FROM ${this.tableName} WHERE id = ${this.dto.id}`);
+    }
 }
