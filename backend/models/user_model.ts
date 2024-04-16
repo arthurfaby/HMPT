f55ee8ead0a5af20337e6e2decb8547fd7d49f30
Arthur Faby <arthurfaby1@gmail.com>
2024-04-16 15:19:11 +0200

    feat(backend:models): refactor properties, set private and create getters and setters

--- backend/models/user_model.ts
+++ backend/models/user_model.ts
@@ -13,148 +13,300 @@ export class User extends AbstractModel<UserDto> {
     /**
      * The user's id
      * @type {number}
-     * @public
+     * @private
      */
-    public id: number;
+    private _id: number;
 
     /**
      * The user's email
      * @type {string}
-     * @public
+     * @private
      */
-    public email: string;
+    private _email: string;
 
     /**
      * The user's username
      * @type {string}
-     * @public
+     * @private
      */
-    public username: string;
+    private _username: string;
 
     /**
      * The user's password
      * @type {string}
-     * @public
+     * @private
      */
-    public password: string;
+    private _password: string;
 
     /**
      * The user's first name
      * @type {string}
-     * @public
+     * @private
      */
-    public firstName: string;
+    private _firstName: string;
 
     /**
      * The user's last name
      * @type {string}
-     * @public
+     * @private
      */
-    public lastName: string;
+    private _lastName: string;
 
     /**
      * The user's gender
      * @type {Gender}
-     * @public
+     * @private
      */
-    public gender: Gender;
+    private _gender: Gender;
 
     /**
      * The user's biography
      * @type {string}
-     * @public
+     * @private
      */
-    public biography: string;
+    private _biography: string;
 
     /**
      * The user's interests
      * @type {string[]}
-     * @public
+     * @private
      */
-    public interests: string[];
+    private _interests: string[];
 
     /**
      * The user's pictures
      * @type {string[]}
-     * @public
+     * @private
      */
-    public pictures: string[];
+    private _pictures: string[];
 
     /**
      * The user's verification status
      * @type {boolean}
-     * @public
+     * @private
      */
-    public verified: boolean;
+    private _verified: boolean;
 
     /**
      * The user's fame rating
      * @type {number}
-     * @public
+     * @private
      */
-    public fameRating: number;
+    private _fameRating: number;
 
     /**
      * The user's geolocation
      * @type {Geolocation}
-     * @public
+     * @private
      */
-    public geolocation: Geolocation;
+    private _geolocation: Geolocation;
 
     /**
      * The user's location sharing status
      * @type {boolean}
-     * @public
+     * @private
      */
-    public acceptLocation: boolean;
+    private _acceptLocation: boolean;
 
     /**
      * The user's age
      * @type {number}
-     * @public
+     * @private
      */
-    public age: number;
+    private _age: number;
 
     /**
      * The user's online status
      * @type {boolean}
-     * @public
+     * @private
      */
-    public online: boolean; 
+    private _online: boolean; 
 
     /**
      * The user's last online date
      * @type {string}
-     * @public
+     * @private
      */
-    public lastOnlineDate: Date;
+    private _lastOnlineDate: Date;
 
+    public get id(): number {
+        return this._id;
+    }
+
+    public set id(value: number) {
+        this.dto.id = value;
+        this._id = value;
+    }
+
+    public get email(): string {
+        return this._email;
+    }
+
+    public set email(value: string) {
+        this.dto.email = value;
+        this._email = value;
+    }
+
+    public get username(): string {
+        return this._username;
+    }
+
+    public set username(value: string) {
+        this.dto.username = value;
+        this._username = value;
+    }
+
+    public get password(): string {
+        return this._password;
+    }
+
+    public set password(value: string) {
+        this.dto.password = value;
+        this._password = value;
+    }
+
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public set firstName(value: string) {
+        this.dto.first_name = value;
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+
+    public set lastName(value: string) {
+        this.dto.last_name = value;
+        this._lastName = value;
+    }
+
+    public get gender(): Gender {
+        return this._gender;
+    }
+
+    public set gender(value: Gender) {
+        this.dto.gender = value;
+        this._gender = value;
+    }
+
+    public get biography(): string {
+        return this._biography;
+    }
+
+    public set biography(value: string) {
+        this.dto.biography = value;
+        this._biography = value;
+    }
+
+    public get interests(): string[] {
+        return this._interests;
+    }
+
+    public set interests(value: string[]) {
+        this.dto.interests = value;
+        this._interests = value;
+    }
+
+    public get pictures(): string[] {
+        return this._pictures;
+    }
+
+    public set pictures(value: string[]) {
+        this.dto.pictures = value;
+        this._pictures = value;
+    }
+
+    public get verified(): boolean {
+        return this._verified;
+    }
+
+    public set verified(value: boolean) {
+        this.dto.verified = value;
+        this._verified = value;
+    }
+
+    public get fameRating(): number {
+        return this._fameRating;
+    }
+
+    public set fameRating(value: number) {
+        this.dto.fame_rating = value;
+        this._fameRating = value;
+    }
+
+    public get geolocation(): Geolocation {
+        return this._geolocation;
+    }
+
+    public set geolocation(value: Geolocation) {
+        this.dto.geolocation = value;
+        this._geolocation = value;
+    }
+
+    public get acceptLocation(): boolean {
+        return this._acceptLocation;
+    }
+
+    public set acceptLocation(value: boolean) {
+        this.dto.accept_location = value;
+        this._acceptLocation = value;
+    }
+
+    public get age(): number {
+        return this._age;
+    }
+
+    public set age(value: number) {
+        this.dto.age = value;
+        this._age = value;
+    }
+
+    public get online(): boolean {
+        return this._online;
+    }
+
+    public set online(value: boolean) {
+        this.dto.online = value;
+        this._online = value;
+    }
+
+    public get lastOnlineDate(): Date {
+        return this._lastOnlineDate;
+    }
+
+    public set lastOnlineDate(value: Date) {
+        this.dto.last_online_date = value.toISOString().split("T")[0];
+        this._lastOnlineDate = value;
+    }
 
     public constructor(dto: UserDto) {
         super(dto, USER_TABLE_NAME);
-        this.id = dto.id;
-        this.email = dto.email;
-        this.username = dto.username;
-        this.password = dto.password;
-        this.firstName = dto.first_name;
-        this.lastName = dto.last_name;
+        this._id = dto.id;
+        this._email = dto.email;
+        this._username = dto.username;
+        this._password = dto.password;
+        this._firstName = dto.first_name;
+        this._lastName = dto.last_name;
         if (!GENDERS.includes(dto.gender)) {
             throw new Error("Invalid gender");
         }
-        this.gender = dto.gender as Gender;
-        this.biography = dto.biography;
-        this.interests = dto.interests;
-        this.pictures = dto.pictures;
-        this.verified = dto.verified;
-        this.fameRating = dto.fame_rating;
+        this._gender = dto.gender as Gender;
+        this._biography = dto.biography;
+        this._interests = dto.interests;
+        this._pictures = dto.pictures;
+        this._verified = dto.verified;
+        this._fameRating = dto.fame_rating;
         if (!(dto.geolocation.latitude && dto.geolocation.longitude)) {
             throw new Error("Invalid geolocation");
         }
-        this.geolocation = dto.geolocation as Geolocation;
-        this.acceptLocation = dto.accept_location;
-        this.age = dto.age;
-        this.online = dto.online;
-        this.lastOnlineDate = new Date(dto.last_online_date);
+        this._geolocation = dto.geolocation as Geolocation;
+        this._acceptLocation = dto.accept_location;
+        this._age = dto.age;
+        this._online = dto.online;
+        this._lastOnlineDate = new Date(dto.last_online_date);
     }
 
     public static select(columns: string[], filters: Filters): Promise<User[]> {
@@ -162,28 +314,28 @@ export class User extends AbstractModel<UserDto> {
     }
 
     public create() {
-        return create("users", this.toDto());
+        return create("users", this.dto);
     }
 
     public toDto(): UserDto {
         return {
             id: this.id,
-            email: this.email,
-            username: this.username,
-            password: this.password,
-            first_name: this.firstName,
-            last_name: this.lastName,
-            gender: this.gender,
-            biography: this.biography,
-            interests: this.interests,
-            pictures: this.pictures,
-            verified: this.verified,
-            fame_rating: this.fameRating,
-            geolocation: this.geolocation,
-            accept_location: this.acceptLocation,
-            age: this.age,
-            online: this.online,
-            last_online_date: this.lastOnlineDate.toISOString().split("T")[0],
+            email: this._email,
+            username: this._username,
+            password: this._password,
+            first_name: this._firstName,
+            last_name: this._lastName,
+            gender: this._gender,
+            biography: this._biography,
+            interests: this._interests,
+            pictures: this._pictures,
+            verified: this._verified,
+            fame_rating: this._fameRating,
+            geolocation: this._geolocation,
+            accept_location: this._acceptLocation,
+            age: this._age,
+            online: this._online,
+            last_online_date: this._lastOnlineDate.toISOString().split("T")[0],
 
         }
     }
