64a8b0cc75f6a993314379f2f1dcbc4003c5d4d6
Arthur Faby <arthurfaby1@gmail.com>
2024-04-16 17:55:47 +0200

    feat(backend:orm): refactor and add update method to abstract model

--- backend/libs/orm/models/abstract_model.ts
+++ backend/libs/orm/models/abstract_model.ts
@@ -1,17 +1,23 @@
 import { AbstractDto } from "../dtos/abstract_dto";
 import query from "../queries/abstract_query";
+import create from "../queries/create_query";
 import select from "../queries/select_query";
 import { Filters } from "../types/filter_type";
+import { getParsedValue } from "../utils/get_parsed_value";
 
 export abstract class AbstractModel<T extends AbstractDto> {
     /**
      * The table name
      * @type {string}
-     * @static
      * @public
      */
     public tableName: string;
 
+    /**
+     * The data transfer object
+     * @type {T}
+     * @public
+     */
     public dto: T;
 
     /**
@@ -25,7 +31,19 @@ export abstract class AbstractModel<T extends AbstractDto> {
         this.tableName = tableName;
     }
 
+    public create() {
+        return create(this.tableName, this.dto);
+    }
+
     public async delete() {
         return await query(`DELETE FROM ${this.tableName} WHERE id = ${this.dto.id}`);
+    }
+
+    public async update() {
+        const keys = Object.keys(this.dto).join(", ");
+        const values = Object.values(this.dto).map(value => {
+            return getParsedValue(value);
+        }).join(', ');
+        return await query(`UPDATE ${this.tableName} SET (${keys}) = (${values}) WHERE id = ${this.dto.id}`);
     }
 }
