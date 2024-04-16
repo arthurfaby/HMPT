4588d813afd07a1f5e2cf969cdd471f990b693e4
Arthur Faby <arthurfaby1@gmail.com>
2024-04-16 15:18:26 +0200

    feat(backend:orm): little refactor

---
+++ backend/libs/orm/utils/get_parsed_value.ts
@@ -0,0 +1,22 @@
+export const getParsedValue = (parsedValue: any): string => {
+    if (typeof parsedValue === "string") {
+        return `'${parsedValue}'`;
+    }
+    if (typeof parsedValue === 'object') {
+        if (Array.isArray(parsedValue)) {
+            return `'{"${parsedValue.join('", "')}"}'`;
+        }
+        const objectKeys = Object.keys(parsedValue);
+        const objectValues = Object.values(parsedValue);
+        let objectAsString = "'{";
+        objectKeys.forEach((objectKey, index) => {
+            objectAsString += `"${objectKey}": "${objectValues[index]}"`
+            if (index !== objectKeys.length - 1) {
+                objectAsString += ", ";
+            }
+        });
+        objectAsString += "}'";
+        return objectAsString;
+    }
+    return parsedValue;
+}
