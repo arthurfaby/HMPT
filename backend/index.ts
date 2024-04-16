99299fca24386ef186a24f90fae54a7b0590cca7
Arthur Faby <arthurfaby1@gmail.com>
2024-04-16 17:56:21 +0200

    fix(backend): change test to update user

--- backend/index.ts
+++ backend/index.ts
@@ -6,10 +6,31 @@ const app = express();
   
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.json()) 
+
+const user = new User({
+    id: 5,
+    email: "sdfsdf",
+    username: "stestst",
+    password: "pass",
+    first_name: "first",
+    last_name: "last",
+    gender: "male",
+    biography: "biography",
+    interests: ["a", "b"],
+    pictures: ["b", "b"],
+    verified: true,
+    fame_rating: 5,
+    geolocation: {latitude: 123, longitude: 32},
+    accept_location: false,
+    age: 21,
+    online: true,
+    last_online_date: '2024-04-12',
+} as UserDto)
+
  
 app.get("/", async (req: Request, res: Response) => {
     try {
-        return res.send(await User.select(["email", "username"], {email: {
+        return res.send(await User.select(["gender"], {email: {
             equal: "test"
         }}));
     
@@ -24,26 +45,7 @@ app.get("/", async (req: Request, res: Response) => {
 
 app.get("/test", async (req: Request, res: Response) => {
     try {
-        const user = new User({
-            email: "email",
-            username: "username",
-            password: "pass",
-            first_name: "first",
-            last_name: "last",
-            gender: "male",
-            biography: "biography",
-            interests: ["a", "b"],
-            pictures: ["a", "b"],
-            verified: true,
-            fame_rating: 5,
-            geolocation: {latitude: 12, longitude: 32},
-            accept_location: false,
-            age: 21,
-            online: true,
-            last_online_date: '2024-04-12',
-        } as UserDto)
-        ;
-        res.status(200).send(await user.create());
+        res.status(200).send(await user.update());
     } catch (error: unknown) {
         if (error instanceof Error) {
             return res.status(500).send({status: 500, error: error.message})
