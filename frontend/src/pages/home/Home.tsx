866abf6dabc94cf4957a50ded4b5507ee5a8c41a
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-05-03 16:04:24 +0200

    fix(front:home): add title to home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="home-page">
       <img src={heart} alt="home" />
+      <h2 className="home-title">Please create an account or log in to access the app.</h2>
     </div>
   );
 };
