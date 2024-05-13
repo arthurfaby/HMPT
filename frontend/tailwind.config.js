9b8767eea490d9ffcf94af14b8848da84afa59f7
Arthur Faby <arthurfaby1@gmail.com>
2024-05-13 14:21:01 +0200

    feat(frontend): add tailwind and put global colors

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10439F",
+        secondary: "#874CCC",
+        tertiary: "#F27BBD",
+      },
+    },
+  },
+  plugins: [],
+}
+
