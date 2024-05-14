76d179e5c00dff37796c8752c8691ddc7905213c
Arthur Faby <arthurfaby1@gmail.com>
2024-05-14 17:53:41 +0200

    feat(frontend): add toaster + prettier

--- frontend/src/layouts/global/global-layout.tsx
+++ frontend/src/layouts/global/global-layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/layouts/global/components/navbar";
 import { Footer } from "@/layouts/global/components/footer";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function GlobalLayout() {
   return (
@@ -8,6 +9,7 @@ export default function GlobalLayout() {
       <Navbar />
       <main>
         <Outlet />
+        <Toaster />
       </main>
       <Footer />
     </>
--- frontend/src/pages/auth/login/components/loginForm.tsx
+++ frontend/src/pages/auth/login/components/loginForm.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent, useEffect } from "react"; 
+import { useState, FormEvent, useEffect } from "react";
 import { getUser, postLogin } from "../../../../services/api/authApi";
 import User from "../../../../types/user";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,39 +6,53 @@ import "../styles/loginForm.css";
 import { AuthStatus, useAuth } from "../../../../hooks/useAuth";
 
 export default function LoginForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const {status, login, Authenticate} = useAuth()
-    console.log(status)
-    useEffect(() => {
-        Authenticate()
-        if (status === AuthStatus.Authenticated) {
-            navigate('/home');
-        }
-    }, [status, navigate]);
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        login(username, password)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { status, login, Authenticate } = useAuth();
+  useEffect(() => {
+    Authenticate();
+    if (status === AuthStatus.Authenticated) {
+      navigate("/");
     }
-    return (
-        <div className="wrapper">
-            <form onSubmit={handleSubmit}>
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder="login" name="username" value={username} onChange={(event) => setUsername(event.currentTarget.value)}/>
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder="password" name="password" value={password} onChange={(event) => setPassword(event.currentTarget.value)}/>
-                </div>
-                <div>
-                    <button type="submit" className="btn">login</button>
-                </div>
-                <div className="link-register">
-                    <p>Don't have an account ?
-                    <Link to='/register'> Register</Link> </p>
-                </div>
-            </form>
+  }, [status, navigate]);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    login(username, password);
+  };
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="login"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.currentTarget.value)}
+          />
         </div>
-        );
+        <div className="input-box">
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" className="btn">
+            login
+          </button>
+        </div>
+        <div className="link-register">
+          <p>
+            Don't have an account ?<Link to="/register"> Register</Link>{" "}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 }
