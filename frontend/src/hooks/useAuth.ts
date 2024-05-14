eb8253e2fd47a6f4794bcd7578e166cb3066b958
Arthur Faby <arthurfaby1@gmail.com>
2024-05-14 17:53:14 +0200

    feat(frontend): add logout logic

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,44 +1,52 @@
-import User from '../types/user';
-import {useAccountStore} from '../store';
-import { useCallback } from 'react';
-import { getUser, postLogin } from '../services/api/authApi';
+import User from "../types/user";
+import { useAccountStore } from "../store";
+import { useCallback } from "react";
+import { getUser, postLogin } from "../services/api/authApi";
+import { toast } from "sonner";
 
 export enum AuthStatus {
-    Unknown = 0,
-    Authenticated = 1,
-    Guest = 2,
+  Unknown = 0,
+  Authenticated = 1,
+  Guest = 2,
 }
 
 export function useAuth() {
-    const { account, setAccount } = useAccountStore();
-    let status = AuthStatus.Unknown;
-    
-    switch(account){
-        case null:
-            status = AuthStatus.Guest;
-            break;
-        case undefined:
-            status = AuthStatus.Unknown;
-            break;
-        default:
-            status = AuthStatus.Authenticated;
-            break;
-    }
+  const { account, setAccount } = useAccountStore();
+  let status = AuthStatus.Unknown;
 
-    const Authenticate = useCallback(async () => {
-        await getUser().then(setAccount)
-        .catch(() => setAccount(null));
-     }, [])
+  switch (account) {
+    case null:
+      status = AuthStatus.Guest;
+      break;
+    case undefined:
+      status = AuthStatus.Unknown;
+      break;
+    default:
+      status = AuthStatus.Authenticated;
+      break;
+  }
 
-     const login = useCallback(async (username: string, password: string) => {
-        await postLogin({username, password}).then(setAccount)
-        console.log(status)
-     }, [])
-    
-    return {
-        account,
-        status,
-        Authenticate,
-        login,
-    };
+  const Authenticate = useCallback(async () => {
+    await getUser()
+      .then(setAccount)
+      .catch(() => setAccount(null));
+  }, []);
+
+  const logout = useCallback(async () => {
+    toast.success("You are now logged out.");
+    setAccount(null);
+  }, []);
+
+  const login = useCallback(async (username: string, password: string) => {
+    await postLogin({ username, password }).then(setAccount);
+    toast.success("You are now logged in.");
+  }, []);
+
+  return {
+    account,
+    status,
+    Authenticate,
+    login,
+    logout,
+  };
 }
