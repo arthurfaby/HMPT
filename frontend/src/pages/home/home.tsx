2083d3aa0ade7adc4a42b76ac515682d0f079cbf
Arthur Faby <arthurfaby1@gmail.com>
2024-05-14 18:47:56 +0200

    feat(frontend): display matches component when logged in on route /

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,8 +1,15 @@
 import heart from "../../assets/heart.webp";
 import "./home.css";
 import { FullHeightContainer } from "@/components/utils/full-height-container";
+import { AuthStatus, useAuth } from "@/hooks/useAuth";
+import { Matches } from "@/pages/matches/matches";
 
 const Home = () => {
+  const { status } = useAuth();
+
+  if (status === AuthStatus.Authenticated) {
+    return <Matches />;
+  }
   return (
     <FullHeightContainer className="home-page flex flex-col items-center justify-center  w-full gap-8 bg-secondary">
       <img src={heart} alt="home" className="h-1/2 aspect-square" />
