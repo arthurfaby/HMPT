9dbd5f6c52297cfacdd57de1b2ad4dfa2af8d491
Arthur Faby <arthurfaby1@gmail.com>
2024-05-17 15:49:48 +0200

    feat(frontend): prettier

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -11,9 +11,9 @@ const Home = () => {
     return <Matches />;
   }
   return (
-    <FullHeightContainer className="home-page flex flex-col items-center justify-center  w-full gap-8 bg-secondary">
-      <img src={heart} alt="home" className="h-1/2 aspect-square" />
-      <h2 className="text-secondary-foreground text-center text-4xl font-bold">
+    <FullHeightContainer className="home-page flex w-full flex-col items-center  justify-center gap-8 bg-secondary">
+      <img src={heart} alt="home" className="aspect-square w-1/2" />
+      <h2 className="text-center text-4xl font-bold text-secondary-foreground">
         Please create an account or log in to access the app.
       </h2>
     </FullHeightContainer>
