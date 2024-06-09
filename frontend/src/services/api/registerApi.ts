f1d192979bee6d6c336c561dbf7963a4283ad829
Arthur <75225926+arthurfaby@users.noreply.github.com>
2024-06-09 16:37:50 +0200

    feat(frontend): create apiUrl config variable and use it

--- frontend/src/services/api/registerApi.ts
+++ frontend/src/services/api/registerApi.ts
@@ -1,5 +1,6 @@
 import ky from "ky";
+import {apiUrl} from '@/config/apiUrl';
 
 export default async function postRegister(user: any): Promise<Response> {
-  return ky.post("http://localhost:5000/register", { json: user });
+  return ky.post(`${apiUrl}/register}`, { json: user });
 }
