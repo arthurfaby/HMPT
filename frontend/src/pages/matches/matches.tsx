3708b0dc959944ecef40ff06e860e281a638efac
Arthur Faby <arthurfaby1@gmail.com>
2024-06-10 16:49:11 +0200

    fix(front): change getUserToMatch route

--- frontend/src/pages/matches/matches.tsx
+++ frontend/src/pages/matches/matches.tsx
@@ -2,12 +2,12 @@ import { FullHeightContainer } from "@/components/utils/full-height-container";
 import { UserDto } from "@/dtos/user_dto";
 import { MatchSwiper } from "@/pages/matches/components/match-swiper";
 import { useEffect, useState } from "react";
-import { getUsers } from "@/services/api/users/getUsers";
+import { getUsersToMatch } from "@/services/api/users/getUsers";
 export function Matches() {
   const [users, setUsers] = useState<UserDto[]>([]);
 
   useEffect(() => {
-    getUsers().then((users) => {
+    getUsersToMatch().then((users) => {
       setUsers(users);
     });
   }, []);
--- frontend/src/services/api/users/getUsers.ts
+++ frontend/src/services/api/users/getUsers.ts
@@ -1,10 +1,18 @@
 import { apiUrl } from "@/config/apiUrl";
 import { UserDto } from "@/dtos/user_dto";
+import ky from "ky";
 
 /**
  * Get all the users
  */
-export async function getUsers(): Promise<UserDto[]> {
-  const response = await fetch(`${apiUrl}/users`);
-  return await response.json();
+export async function getUsersToMatch(): Promise<UserDto[]> {
+  const data = await ky
+    .get(`${apiUrl}/matches/usersToMatch`, {
+      credentials: "include",
+    })
+    .json<{ error: string } | UserDto[]>();
+
+  if (Array.isArray(data)) return data;
+  //TODO: handle error
+  throw new Error(data.error);
 }
