86ba8024608427c5a5636c5470917b20e4a40e38
Arthur Faby <arthurfaby1@gmail.com>
2024-06-10 16:47:44 +0200

    fix(backend): add error key to unauthorized

--- backend/app/routes/me.route.ts
+++ backend/app/routes/me.route.ts
@@ -4,7 +4,10 @@ import getAuthenticatedUser from "../utils/auth/getAuthenticatedUser";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const user = await getAuthenticatedUser(req.sessionID)
+  const user = await getAuthenticatedUser(req.sessionID);
+  if (!user) {
+    return res.status(401).send({ error: "Unauthorized" });
+  }
   return res.status(200).send(user.dto);
 });
 
