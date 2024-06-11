dab4759fded02296b832f4407f995ccdedbb681b
Arthur Faby <arthurfaby1@gmail.com>
2024-06-11 15:24:05 +0200

    fix(frontend): remove bad idea

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -5,11 +5,6 @@ import { AuthStatus, useAuth } from "@/hooks/useAuth";
 import { Matches } from "@/pages/matches/matches";
 
 const Home = () => {
-  const { status } = useAuth();
-
-  if (status === AuthStatus.Authenticated) {
-    return <Matches />;
-  }
   return (
     <FullHeightContainer className="home-page flex w-full flex-col items-center  justify-center gap-8 bg-secondary">
       <img
