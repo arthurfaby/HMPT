b7ab6c4b4aac7129d8d7b9bd13f082702516a6a2
Arthur Faby <arthurfaby1@gmail.com>
2024-06-11 15:02:42 +0200

    feat(frontend): add unauth guard

---
+++ frontend/src/pages/auth/authGuards.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren, useEffect } from "react";
+import { AuthStatus, useAuth } from "../../hooks/useAuth";
+import { Navigate, Outlet } from "react-router-dom";
+
+export function AuthGuard(props: PropsWithChildren) {
+  const { status, Authenticate } = useAuth();
+
+  if (status === AuthStatus.Unknown) {
+    Authenticate();
+  }
+
+  if (status === AuthStatus.Guest) {
+    return <Navigate to="/login" replace={true}></Navigate>;
+  }
+  return <Outlet />;
+}
+
+export function UnAuthGuard(props: PropsWithChildren) {
+  const { status } = useAuth();
+
+  if (status === AuthStatus.Authenticated) {
+    return <Navigate to="/matches" replace={true}></Navigate>;
+  }
+  return <Outlet />;
+}
