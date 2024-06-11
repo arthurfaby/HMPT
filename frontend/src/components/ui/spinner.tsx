f7f112d828f01da5e69adcd25141e5bf08f20fbd
Arthur Faby <arthurfaby1@gmail.com>
2024-06-11 16:14:23 +0200

    fix(frontend): change loading icon

--- frontend/src/components/ui/spinner.tsx
+++ frontend/src/components/ui/spinner.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
-import { Loader2 } from "lucide-react";
+import {
+  Heart,
+  MessageCircle,
+  MessageCircleHeart,
+  MessageSquareHeart,
+} from "lucide-react";
 
 const spinnerVariants = cva("flex-col items-center justify-center", {
   variants: {
@@ -43,7 +48,7 @@ export function Spinner({
 }: SpinnerContentProps) {
   return (
     <span className={spinnerVariants({ show })}>
-      <Loader2 className={cn(loaderVariants({ size }), className)} />
+      <Heart className={cn(loaderVariants({ size }), className)} />
       {children}
     </span>
   );
