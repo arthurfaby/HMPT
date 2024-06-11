bf9449874d717808429b56547e52b1fad7212e48
Arthur Faby <arthurfaby1@gmail.com>
2024-06-11 14:48:01 +0200

    feat(backend): add likeuser route

--- backend/app/routes/matches/index.ts
+++ backend/app/routes/matches/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users_to_match from "./users-to-match";
+import like_user from "./like-user";
 
 const router = Router();
 
 router.use("/", users_to_match);
+router.use("/", like_user);
 
 export default router;
