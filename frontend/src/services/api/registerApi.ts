419f503ce2a084837a652c8df3dbb2b688e3317d
Arthur Faby <arthurfaby1@gmail.com>
2024-07-01 21:09:49 +0200

    feat(frontend): error handling register

--- frontend/src/services/api/registerApi.ts
+++ frontend/src/services/api/registerApi.ts
@@ -6,7 +6,7 @@ export default async function postRegister(
   password: string,
   firstName: string,
   lastname: string,
-) {
+) : Promise<{ error: string } | { message: string }>{
   return (
     await ky.post("http://localhost:5000/register", {
       json: { username, email, password, firstName, lastname },
